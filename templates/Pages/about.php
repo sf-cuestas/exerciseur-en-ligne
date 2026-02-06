b86b65f682fbde327c6485db4b0f825d75384e82
p2411304 <jerome.tardivon@etu.univ-lyon1.fr>
2026-02-06 11:47:24 +0100

    changed little h to big H

--- templates/Pages/about.php
+++ templates/Pages/about.php
@@ -5,6 +5,6 @@
 
     <p>
         Ce site web a été réalisé dans le cadre d'un projet étudiant de 2eme année de BUT Informatique, à l'IUT Lyon 1 de Bourg-en-Bresse. <br>
-        La police d'écriture utilisée est <?= $this->html->link('Luciole', 'https://luciole-vision.com/')?>, une police spécialement créée pour les personnes malvoyantes.
+        La police d'écriture utilisée est <?= $this->Html->link('Luciole', 'https://luciole-vision.com/')?>, une police spécialement créée pour les personnes malvoyantes.
     </p>
 </main>
